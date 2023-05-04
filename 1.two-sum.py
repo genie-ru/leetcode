5a78de3262140b13f7ee7aa739e29c2d7b6422ba
genie-ru <genie.zgmf987@gmail.com>
2023-05-05 04:51:57 +0900

    add

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,15 +7,16 @@
 # @lc code=start
 from typing import List
 class Solution:
-   def twoSum(self, nums: List[int], target: int) -> List[int]:
-       seen = {}
-       for i, value in enumerate(nums): #1
-           remaining = target - nums[i] #2
-           
-           if remaining in seen: #3
-               return [i, seen[remaining]]  #4
-           else:
-               seen[value] = i  #5
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            if target - num in seen:
+                return [seen[target - num], i]
+            else:
+                seen[num] = i
+        return []
+
+
         
 # @lc code=end
 
