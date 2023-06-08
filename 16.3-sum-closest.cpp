f2129fc71d0016e80c866797d55d348e77e3705c
genie-ru <genie.zgmf987@gmail.com>
2023-06-08 20:13:28 +0900

    latest

--- 16.3-sum-closest.cpp
+++ 16.3-sum-closest.cpp
@@ -82,3 +82,10 @@ private:
 
 // @lc code=end
 
+// ヘッダーファイル <vector> と <algorithm> をインクルードしました。
+// Python の List 型は、C++ では std::vector を使用します。
+// Python の sum() 関数は、C++ ではループを使用して合計を計算しました。
+// Python の abs() 関数は、C++ では std::abs() を使用しました。
+// Python の enumerate() 関数は、C++ ではインデックスを使用してループしました。
+// Python の key パラメーターを使用した min() 関数は、C++ では手動で最小値を追跡するループを使用しました。
+// この C++ コードは、Python の Solution クラスの threeSumClosest メソッドと KSumClosest メソッドを再現します。ご参考までにご利用ください。
